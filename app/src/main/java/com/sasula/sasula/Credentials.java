14896aaf1b279c68576e85409b9c1b815d955bf7
haggaibrian <haggzo@gmail.com>
2019-01-08 17:30:49 +0300

    trial

---
+++ app/src/main/java/com/sasula/sasula/Credentials.java
@@ -0,0 +1,13 @@
+package com.sasula.sasula;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Credentials extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credentials);
+    }
+}
